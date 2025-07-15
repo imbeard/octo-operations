7f8af178b13085e997a889c01e2d601f9799d0d8
millimetro_ <103439094+millimetro@users.noreply.github.com>
2025-07-15 19:22:42 +0200

    small fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,18 +36,18 @@ export default async function Home() {
       <Header />
 
       {/* Main content */}
-      <main className="min-h-screen bg-white mt-[18vh] md:mt-[15vh] md:overflow-hidden">
+      <main className="min-h-screen md:h-screen bg-white pt-[18vh] md:pt-[15vh]">
         <div className="mx-auto px-3 h-full">
           <div className="flex h-full flex-col md:flex-row">
             {/* Projects Section - 4/5 width */}
             <div className="md:w-3/4 flex flex-col">
-              <div className="relative">
+              <div className="relative md:overflow-y-scroll h-full md:h-[85vh]">
                 <Link href="/projects">
                   <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4 uppercase sticky top-[15vh] md:top-[0vh] z-10">
                     Projects
                   </h1>
                 </Link>
-                <div className="flex-1 md:overflow-y-auto py-5">
+                <div className="flex-1 overflow-y-scroll py-5">
                   <Projects projects={projects} />
                 </div>
               </div>
@@ -55,13 +55,13 @@ export default async function Home() {
 
             {/* Labs Section - 1/5 width */}
             <div className="md:w-1/4 flex flex-col">
-              <div className="relative ">
+              <div className="relative h-full md:h-[85vh] md:overflow-y-scroll">
                 <Link href="/lab">
                   <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4 uppercase sticky top-[15vh] md:top-[0vh] z-10">
                     OCTO Lab
                   </h1>
                 </Link>
-                <div className="flex-1 md:overflow-y-auto py-5 ">
+                <div className="flex-1 overflow-y-scroll py-5 ">
                   <Blog />
                 </div>
               </div>
