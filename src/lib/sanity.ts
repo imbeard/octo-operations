e47eb63a8f51fd710c7cbb8b9d687e5957e2fe7a
millimetro_ <103439094+millimetro@users.noreply.github.com>
2025-07-28 22:05:12 +0200

    up

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,8 +4,8 @@ import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 // Sanity client configuration
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'a2claayl',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'a2claayl',
+  dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   apiVersion: '2025-02-19',
   useCdn: process.env.NODE_ENV === 'production',
   perspective: 'published',
